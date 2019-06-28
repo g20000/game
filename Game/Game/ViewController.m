1722d830992e723ab6313f4f559e7bf47f7d3212
Andrew Bobkov <pragmus2@gmail.com>
2019-06-28 21:09:29 +0300

    initial project

---
+++ Game/Game/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Game
+//
+//  Created by pragmus on 28/06/2019.
+//  Copyright © 2019 pragmus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
